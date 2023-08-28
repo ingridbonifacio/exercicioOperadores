15c7eb115518b0971776fe365c47267e2c166373
ingridbonifacio <117772818+ingridbonifacio@users.noreply.github.com>
2023-08-27 21:54:57 -0300

    Add files via upload

---
+++ exercicio2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main(){
+
+    /*Escreva um prgrama que receba um numero qualquer e mostre o dobro.*/
+
+    //declaração de variaveis 
+
+    int numero, dobro;
+
+    //entrada de dados
+
+    printf("Digite um numero");
+    scanf("%d", &numero);
+
+    //processamento dos dados
+    dobro =  numero * 2;
+
+    printf("O dobro do numero digitado é: %d", dobro);
+    
+
+
+}
